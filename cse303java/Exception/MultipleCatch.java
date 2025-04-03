7255fe2c9dc91d1890da03b3c4f3df1b6da36dfb
23-303-mvsrec <245123733303@mvsrec.edu.in>
2025-04-03 05:00:07 +0000

    indentation

--- cse303java/Exception/MultipleCatch.java
+++ cse303java/Exception/MultipleCatch.java
@@ -1,37 +1,30 @@
 class MultipleCatch {
     public static void main(String[] args) {
         System.out.println("Main method started");
-        int a = 5,b = 0,c = 0;
-        int arr[] = {10, 20, 30};
-        String str ="abc";
+        int a = 5, b = 0, c = 0;
+        int arr[] = { 10, 20, 30 };
+        String str = "abc";
         try {
-            c = a/b;
+            c = a / b;
             System.out.println("Result: " + c);
             System.out.println(arr[5]); // This will cause an ArrayIndexOutOfBoundsException
-            System.out.println(str.charAt(5)); // This will cause a StringIndexOutOfBoundsException 
+            System.out.println(str.charAt(5)); // This will cause a StringIndexOutOfBoundsException
             int k = Integer.parseInt(args[0]); // This will cause a NumberFormatException
-            System.out.println("Parsed integer: " + k); 
+            System.out.println("Parsed integer: " + k);
             System.out.println("After error in try block");
-        }
-        catch (ArithmeticException e) {
+        } catch (ArithmeticException e) {
             System.out.println("ArithmeticException caught: " + e.getMessage());
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("ArrayIndexOutOfBoundsException caught: " + e.getMessage());
-        }
-        catch (StringIndexOutOfBoundsException e) {
+        } catch (StringIndexOutOfBoundsException e) {
             System.out.println("StringIndexOutOfBoundsException caught: " + e.getMessage());
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println("NumberFormatException caught: " + e.getMessage());
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("General Exception caught: " + e.getMessage());
-        }
-        finally {
+        } finally {
             System.out.println("Finally block executed");
         }
         System.out.println("Main method ended");
     }
 }
-
